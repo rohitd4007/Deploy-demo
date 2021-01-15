4e77c6c80f4a1d8862c1a5bed3b09aad728bb5a2
rohitd4007 <44453539+rohitd4007@users.noreply.github.com>
2021-01-15 14:48:37 +0530

    update to dep

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hacked-recipe'));
+app.use(express.static(__dirname + '../hacked-recipe', ));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/hacked-recipe/index.html'));
+    res.sendFile(path.join(__dirname + '../hacked-recipe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
