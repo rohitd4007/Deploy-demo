f68f1d4b2365064c70c8bb36f9f39b881e5dc1ac
rohitd4007 <44453539+rohitd4007@users.noreply.github.com>
2021-01-15 15:57:48 +0530

    updateuuuoooo

--- server.js
+++ server.js
@@ -21,17 +21,19 @@
 //     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 // });
 
+
+
 // const express = require('express');
 // const path = require('path');
 
 // const app = express();
 
 // // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + './src'));
+// app.use(express.static(__dirname + '/src'));
 
 // app.get('/*', function(req, res) {
 
-//     res.sendFile('index.html', { root: './src' });
+//     res.sendFile('index.html', { root: '/src' });
 // });
 
 
